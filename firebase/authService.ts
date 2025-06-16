ac26218ddcd3b748347638ab3dfa07468c34303d
Erick Abraham Garcia Maciel <egarciamaciel1@gmail.com>
2025-06-16 15:58:07 -0600

    Tercer commit del proyecto mascotas

--- firebase/authService.ts
+++ firebase/authService.ts
@@ -1,18 +1,43 @@
 //import { initializeApp } from 'firebase/app';
+//import { initializeApp, getApps, getApp } from 'firebase/app';
+//import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+//import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+//import { Platform } from 'react-native';
+//import { firebaseConfig } from './firebaseConfig';
+
+//const app = initializeApp(firebaseConfig);
+//const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+
+//let auth;
+
+//if (Platform.OS === 'web') {
+//  auth = getAuth(app); // Para web usamos getAuth directo
+//} else {
+//  auth = initializeAuth(app, {
+//    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+//  });
+//}
+
+//export { auth };
+
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+  Auth,
+} from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 import { firebaseConfig } from './firebaseConfig';
 
-//const app = initializeApp(firebaseConfig);
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-
-let auth;
+let auth: Auth;
 
 if (Platform.OS === 'web') {
-  auth = getAuth(app); // Para web usamos getAuth directo
+  auth = getAuth(app); // En web no se requiere AsyncStorage
 } else {
   auth = initializeAuth(app, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage),
@@ -20,3 +45,4 @@ if (Platform.OS === 'web') {
 }
 
 export { auth };
+
